e597c6a76f920855ac954d5886f8c39e8c3f4213
Nerinne <ire.3n3r1@gmail.com>
2026-02-16 12:23:08 +0100

    Update nivel1.java

--- nivel1.java
+++ nivel1.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class nivel1{
     public static void main(String[] args)throws IOException, InterruptedException{
-        limpiar.limpiar();
+        limpiar.clear();
 
         int S1 = ((int)(10*Math.random())) +1, 
         S2 = (int)(20 + ((30-20+1)*Math.random())), 
@@ -10,17 +11,15 @@ public class nivel1{
 
         Scanner sc = new Scanner(System.in);
 
-        for(int i = S1, i <= S2; i++){
+        for(int i = S1; i <= S2; i++){
             solucion += i;
         }
 
-        System.out.println("Los problemas empiezan cuando deben realizar un salto hiperespacial hasta al
-sistema" + S1 + "en el sector " + S2 + ", pero el sistema de navegación está estropeado y el
-computador tiene problemas para calcular parte de las coordenadas de salto.\n
-Chewbacca, piloto experto, se da cuenta que falta el cuarto número de la serie.\n
-Recuerda de sus tiempos en la academia de pilotos que para calcularlo hay que
-calcular el sumatorio entre el nº del sistema y el nº del sector (ambos inclusive).\n
-¿Qué debe introducir?");
+        System.out.println("Los problemas empiezan cuando deben realizar un salto hiperespacial hasta al sistema " + S1 + "en el sector " + S2 + ", pero el sistema de navegación está estropeado y el computador tiene problemas para calcular parte de las coordenadas de salto.\n" + 
+        "Chewbacca, piloto experto, se da cuenta que falta el cuarto número de la serie.\n" + 
+        "Recuerda de sus tiempos en la academia de pilotos que para calcularlo hay que calcular el sumatorio entre el nº del sistema y el nº del sector (ambos inclusive).\n" + 
+        "¿Qué debe introducir?");
+
         respuesta = sc.nextInt();
         if(respuesta == solucion){
             System.out.println("Correcto! Ahora podremos llegar al sector correcto!");
@@ -28,6 +27,7 @@ calcular el sumatorio entre el nº del sistema y el nº del sector (ambos inclus
             nivel2.main(new String[]{});
         } else
             perder.main(new String[]{});
-        
+
+        sc.close();
     }
 }
