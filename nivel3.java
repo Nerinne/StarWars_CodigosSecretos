f8b8ecb5b3071c4e8e9165dc03f17219dcff04c1
Nerinne <ire.3n3r1@gmail.com>
2026-02-16 12:23:27 +0100

    Update nivel3.java

--- nivel3.java
+++ nivel3.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
+import java.util.Scanner;import java.io.*;
 
 public class nivel3{
     public static void main(String[] args)throws IOException, InterruptedException{
-        limpiar.limpiar();
+        limpiar.clear();
         
         int N = (int)(50 + ((100-50+1)*Math.random())), 
         respuesta = 0, solucion = 1;
 
         Scanner sc = new Scanner(System.in);
 
-        for(int i = 1, i <= (N/10); i++){ //factorial de n/10
+        for(int i = 1; i <= (N/10); i++){ //factorial de n/10
             solucion *= i;
         }
 
-        System.out.println("Han Solo proporciona el código correcto.\n Atracan en la estrella de la muerte, se
-equipan con trajes de soldados imperiales que encuentran en la nave para pasar
-desapercibidos y bajan.\n Ahora deben averiguar en qué nivel de los " + N +" existentes se
-encuentra el reactor principal.\n Se dirigen al primer panel computerizado que
-encuentran y la Princesa Leia intenta acceder a los planos de la nave pero necesita
-introducir una clave de acceso.\n Entonces recuerda la información que le proporcionó
-Lando Calrissian:\n “La clave de acceso a los planos de la nave es el factorial de " + N + "/10
-(redondeando N hacia abajo), donde " + N + " es el nº de niveles”.\n
-¿Cual es el nivel correcto?");
+        System.out.println("Han Solo proporciona el código correcto.\n" + 
+        "Atracan en la estrella de la muerte, se equipan con trajes de soldados imperiales que encuentran en la nave para pasar desapercibidos y bajan.\n" + 
+        "Ahora deben averiguar en qué nivel de los " + N + " existentes seencuentra el reactor principal.\n" + 
+        "Se dirigen al primer panel computerizado que encuentran y la Princesa Leia intenta acceder a los planos de la nave pero necesita introducir una clave de acceso.\n" + 
+        "Entonces recuerda la información que le proporcionó Lando Calrissian:\n" + 
+        "“La clave de acceso a los planos de la nave es el factorial de " + N + " /10 (redondeando N hacia abajo), donde " + N + 
+        "es el nº de niveles”.\n" + 
+        "¿Cual es el nivel correcto?");
 
         respuesta = sc.nextInt();
         if(respuesta == solucion){
@@ -30,6 +29,6 @@ Lando Calrissian:\n “La clave de acceso a los planos de la nave es el factoria
             nivel4.main(new String[]{});
         } else
             perder.main(new String[]{});
-        
+        sc.close();
     }
 }
