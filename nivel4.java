53879f28c9303d85715b2c21f7f5ba88193f252f
Nerinne <ire.3n3r1@gmail.com>
2026-02-16 12:23:41 +0100

    Update nivel4.java

--- nivel4.java
+++ nivel4.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class nivel4{
     public static void main(String[] args)throws IOException, InterruptedException{
-        limpiar.limpiar();
+        limpiar.clear();
         
         int P = (int)(10 + ((100-10+1)*Math.random())), 
         respuesta = 0, solucion = 1;
@@ -19,11 +20,10 @@ public class nivel4{
         else
             solucion = 1;
 
-        System.out.println("Gracias a la inteligencia de Leia llegan al nivel correcto y encuentran la puerta
-acorazada que da al reactor principal.\n R2D2 se conecta al panel de acceso para
-intentar hackear el sistema y abrir la puerta.\n Para desencriptar la clave necesita
-verificar si el número " + P + " es primo o no. Si es primo introduce un 1, si no lo es
-introduce un 0");
+        System.out.println("Gracias a la inteligencia de Leia llegan al nivel correcto y encuentran la puerta acorazada que da al reactor principal.\n" + 
+        "R2D2 se conecta al panel de acceso para intentar hackear el sistema y abrir la puerta.\n" + 
+        "Para desencriptar la clave necesita verificar si el número " + P + 
+        "es primo o no. Si es primo introduce un 1, si no lo es introduce un 0");
 
         respuesta = sc.nextInt();
         if(respuesta == solucion){
@@ -32,6 +32,6 @@ introduce un 0");
             nivel5.main(new String[]{});
         } else
             perder.main(new String[]{});
-        
+        sc.close();
     }
 }
