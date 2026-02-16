ebf939327d94cc0c055dc7953da2673c269f37b6
Nerinne <ire.3n3r1@gmail.com>
2026-02-16 12:24:07 +0100

    Update perder.java

--- perder.java
+++ perder.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class fin{
+public class perder{
     public static void main(String[] args)throws IOException, InterruptedException{
-        limpiar.limpiar();
-        System.out.println("Ese no era el código correcto…\n La misión ha sido un fracaso…\n :( :( :(
-Todavía no eres un Maestro Jedi de las Matemáticas.\n ¡Vuelve a intentarlo!");
+        limpiar.clear();
+        System.out.println("Ese no era el código correcto…\n" + " La misión ha sido un fracaso…\n" + 
+        " :( :( :( Todavía no eres un Maestro Jedi de las Matemáticas.\n" + 
+        "¡Vuelve a intentarlo!");
 
         fin.main(new String[]{});
     }
