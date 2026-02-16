1150e3fbd31006b88da5e1816e36f67906486195
Nerinne <ire.3n3r1@gmail.com>
2026-02-16 12:22:59 +0100

    Update limpiar.java

--- limpiar.java
+++ limpiar.java
@@ -1,5 +1,7 @@
+import java.io.*;
+
 public class limpiar{
-    public void limpiar() throws IOException, InterruptedException {
+    public static void clear() throws IOException, InterruptedException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
 }
