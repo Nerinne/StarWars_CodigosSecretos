826d27c39410889daa86adaaa0c703daa15e5451
Nerinne <ire.3n3r1@gmail.com>
2026-02-23 08:37:11 +0100

    :)

--- inicio.java
+++ inicio.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class inicio {
   public static void main(String[] args)throws IOException, InterruptedException{
+    limpiar.limpiar();
     Scanner sc = new Scanner(System.in);
     
     System.out.println("=== STAR WARS CÓDIGOS SECRETOS ===\n" + 
